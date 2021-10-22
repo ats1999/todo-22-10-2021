3ec84197ba42a51afa573631c141750ebc486004
unknown <sagrawal9689@gmail.com>
2021-10-22 20:25:59 +0530

    Readme added

--- server/server.js
+++ server/server.js
@@ -48,13 +48,9 @@ app.use('/api/tasks',taskRoutes)
 
 const __dirname = path.resolve()
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
-}
+app.get('*', (req, res) =>
+    res.status(404).send()
+)
 
 app.use(globalErrorHandler)
 
